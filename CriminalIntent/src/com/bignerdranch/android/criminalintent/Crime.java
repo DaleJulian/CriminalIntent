2168f3be3e28af70ff4b64d684f35e582775348c
Dale Julian <dale@UNIT021.(none)>
2015-06-01 17:19:22 +0800

    Implemented Implicit Intents

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -13,6 +13,7 @@ public class Crime {
     private static final String JSON_DATE = "date";
     private static final String JSON_SOLVED = "solved";
     private static final String JSON_PHOTO = "photo";
+    private static final String JSON_SUSPECT = "suspect";
     
         
     private UUID mId;
@@ -20,6 +21,7 @@ public class Crime {
     private Date mDate;
     private boolean mSolved;
     private NewPhoto mPhoto;
+    private String mSuspect;
     
     public Crime() {
         mId = UUID.randomUUID();
@@ -35,6 +37,8 @@ public class Crime {
         if(json.has(JSON_PHOTO)){
         	mPhoto = new NewPhoto(json.getJSONObject(JSON_PHOTO));
         }
+        if (json.has(JSON_SUSPECT))
+        	mSuspect = json.getString(JSON_SUSPECT);
     }
 
     public JSONObject toJSON() throws JSONException {
@@ -43,9 +47,10 @@ public class Crime {
         json.put(JSON_TITLE, mTitle);
         json.put(JSON_DATE, mDate.getTime());
         json.put(JSON_SOLVED, mSolved);
-        if(mPhoto != null)
+        if(mPhoto != null){
         	json.put(JSON_PHOTO, mPhoto.toJSON());
-        
+        	json.put(JSON_SUSPECT, mSuspect);
+        }
         return json;
     }
 
@@ -89,6 +94,14 @@ public class Crime {
     public void setPhoto(NewPhoto photo){
     	mPhoto = photo;
     }
+
+    public String getSuspect() {
+    	return mSuspect;
+    }
     
+    public void setSuspect(String suspect) {
+    	mSuspect = suspect;
+    }
+
     
 }
